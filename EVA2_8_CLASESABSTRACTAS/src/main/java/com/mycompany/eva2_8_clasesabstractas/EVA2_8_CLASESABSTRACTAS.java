7f0e7efc7b08e1abe895e4594cce06c6ec2c7eb3
ReneDc88 <158646500+ReneDc88@users.noreply.github.com>
2024-04-14 16:50:01 -0600

    Add files via upload

---
+++ EVA2_8_CLASESABSTRACTAS/src/main/java/com/mycompany/eva2_8_clasesabstractas/EVA2_8_CLASESABSTRACTAS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_8_clasesabstractas;
+
+/**
+ *
+ * @author rener
+ */
+public class EVA2_8_CLASESABSTRACTAS {
+
+    public static void main(String[] args) {
+        
+    }
+}
